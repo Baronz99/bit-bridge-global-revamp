f2030ddaf04b3aaf09cc1e9d8225d3027aa10df0
baronz99 <tubotuxstacy@yahoo.com>
2025-12-02 00:13:58 +0100

    Fix UserProfileSerializer syntax and document URL helpers

--- backend/bit-bridge-backend/app/serializers/user_profile_serializer.rb
+++ backend/bit-bridge-backend/app/serializers/user_profile_serializer.rb
@@ -5,7 +5,7 @@ class UserProfileSerializer < ActiveModel::Serializer
              :first_name,
              :last_name,
              :phone_number,
-             :date_of_birth,          # 👈 already used on ProfilePage
+             :date_of_birth,          # used on ProfilePage
              :address_line1,
              :address_line2,
              :city,
@@ -13,11 +13,8 @@ class UserProfileSerializer < ActiveModel::Serializer
              :country,
              :postal_code,
              :proof_of_address_type,
-             :id_document_url,        # 👈 NEW – read-only URL for ID doc
-             :proof_of_address_url    # 👈 NEW – read-only URL for proof of address
-
-  # If you don't want to expose the full user object, keep this commented out
-  # has_one :user
+             :id_document_url,        # read-only URL for ID doc
+             :proof_of_address_url    # read-only URL for proof of address
 
   # --- Helpers for document URLs ---
 
@@ -25,22 +22,17 @@ class UserProfileSerializer < ActiveModel::Serializer
     return unless object.respond_to?(:id_document) && object.id_document.attached?
 
     host = ENV['STAGING_BACKEND_HOST'] || ENV['BACKEND_HOST'] || 'localhost:3000'
+    return unless host.present?
+
     Rails.application.routes.url_helpers.rails_blob_url(object.id_document, host: host)
   end
 
   def proof_of_address_url
     return unless object.respond_to?(:proof_of_address) && object.proof_of_address.attached?
 
-    host = ENV.fetch('STAGING_BACKEND_HOST', ENV.fetch('BACKEND_HOST', nil))
+    host = ENV['STAGING_BACKEND_HOST'] || ENV['BACKEND_HOST'] || 'localhost:3000'
+    return unless host.present?
 
-return unless host.present?
-
-    def id_document_url
-  return unless object.id_document.attached?
-
-  host = ENV['STAGING_BACKEND_HOST'] || ENV['BACKEND_HOST']
-  return unless host.present?
-
-  Rails.application.routes.url_helpers.rails_blob_url(object.id_document, host: host, protocol: "https")
-end
+    Rails.application.routes.url_helpers.rails_blob_url(object.proof_of_address, host: host)
+  end
 end
