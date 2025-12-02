df7869f18858267bb44b7fe1f47bf2716c8789bd
baronz99 <tubotuxstacy@yahoo.com>
2025-12-02 23:48:57 +0100

    new cors

--- backend/bit-bridge-backend/config/initializers/cors.rb
+++ backend/bit-bridge-backend/config/initializers/cors.rb
@@ -3,9 +3,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'http://localhost:3000',
-            'http://localhost:5173',
-            'https://keen-gelato-4f27c8.netlify.app',
-            'poetic-figolla-35655e.netlify.app'
+        'http://localhost:5173',
+        'https://keen-gelato-4f27c8.netlify.app',
+        'https://poetic-figolla-35655e.netlify.app'
+
 
     resource '*',
              headers: :any,
