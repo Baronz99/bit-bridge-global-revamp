9d3e0e64bdf7ecdfa652181f2957f972998683a3
baronz99 <tubotuxstacy@yahoo.com>
2026-01-20 18:18:04 +0100

    cors fix

--- backend/bit-bridge-backend/config/initializers/cors.rb
+++ backend/bit-bridge-backend/config/initializers/cors.rb
@@ -22,7 +22,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
 
     resource "/api/*",
-             headers: %w[Accept Authorization Content-Type Bit-Refresh-Token],
+             headers: %w[Accept Authorization Content-Type Bit-Refresh-Token Idempotency-Key],
              methods: %i[get post put patch delete options head],
              credentials: false
   end
